afea3242baed2f980b39b7945dedac1a170f5135
jiangshanmeta <540118044@qq.com>
2026-02-17 20:03:44 +0800

    feat: download stock data

---
+++ download_stock_data.py
@@ -0,0 +1,11 @@
+import yfinance as yf
+
+
+def download_stock_data():
+    stock_data = yf.download(["AAPL", "FE", "WMT"], "2000-01-03", "2023-12-31")
+    if stock_data is not None:
+        adj_data = stock_data["Adj Close"]
+        adj_data.to_csv("stock_price.csv")
+
+if __name__ == '__main__':
+    download_stock_data()
