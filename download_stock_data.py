93bcd76cd8407a48458a4cb04233ad61abbbbd45
jiangshanmeta <540118044@qq.com>
2026-02-17 20:13:59 +0800

    fix: disable auto_adjust

--- download_stock_data.py
+++ download_stock_data.py
@@ -1,8 +1,7 @@
 import yfinance as yf
 
-
 def download_stock_data():
-    stock_data = yf.download(["AAPL", "FE", "WMT"], "2000-01-03", "2023-12-31")
+    stock_data = yf.download(["AAPL", "FE", "WMT"], "2000-01-03", "2023-12-31",auto_adjust=False)
     if stock_data is not None:
         adj_data = stock_data["Adj Close"]
         adj_data.to_csv("stock_price.csv")
